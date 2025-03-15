716e6a6285d2022c934c99b6898d4b0b06a3a916
Monisha Mekala <monishasmekala@gmail.com>
2025-03-14 22:28:15 -0700

    Hi Luis message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Luis Aguilar!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          How are you?
+        </p>
+        <p>
+          Hope you are doing great!
+        </p>
       </header>
     </div>
   );
